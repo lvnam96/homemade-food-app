c949ade0de8767498a8102f1e38698f1a6fb8e9a
lvnam96 <lvnam96@gmail.com>
2024-11-14 22:52:05 +0700

    build(config): setup `husky` config to auto exec commit-related tasks

---
+++ commitlint.config.ts
@@ -0,0 +1,8 @@
+import type { UserConfig } from '@commitlint/types';
+// import {RuleConfigSeverity} from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+};
+
+export default config;
