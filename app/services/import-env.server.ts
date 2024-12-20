dde688a03c5e08ced877228f51c632718cb2f895
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2024-12-20 23:40:09 +0700

    fix: disable `dotenvx`'s strict mode for non-development environments:
    Prod env normally does not have `.env` files (Remix also discourages
    committing it).

--- app/services/import-env.server.ts
+++ app/services/import-env.server.ts
@@ -3,7 +3,7 @@ import dotenvx from '@dotenvx/dotenvx';
 dotenvx.config({
   quiet: process.env.DEBUG_ENV !== 'true',
   path: ['.env'],
-  strict: true,
+  strict: process.env.NODE_ENV === 'development',
   overload: true,
   debug: process.env.DEBUG_ENV === 'true',
 });
