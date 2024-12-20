994d96169f7bce8a3b7056bd9324a2296a5e67ec
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2024-12-21 00:07:53 +0700

    fix: rework commit dde688a03c5e08ced877228f51c632718cb2f895
    Ref: https://github.com/dotenvx/dotenvx/issues/484

--- app/services/import-env.server.ts
+++ app/services/import-env.server.ts
@@ -4,6 +4,7 @@ dotenvx.config({
   quiet: process.env.DEBUG_ENV !== 'true',
   path: ['.env'],
   strict: process.env.NODE_ENV === 'development',
+  ignore: process.env.NODE_ENV === 'development' ? undefined : ['MISSING_ENV_FILE'],
   overload: true,
   debug: process.env.DEBUG_ENV === 'true',
 });
