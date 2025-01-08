e6f50be1f36aa9e3a6ddbd2123eeb8b6c19cd432
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2025-01-08 22:07:31 +0700

    feat: implement new server-only service `password`

--- app/services/password.server.ts
+++ app/services/password.server.ts
@@ -1,3 +1,25 @@
-export const hash = () => {};
-export const compare = () => {};
-export const getSaltedPasswd = () => {};
+import { compare as bcryptCompare, hash as bcryptHash } from 'bcrypt';
+import { getStrongCryptoRandomStr } from '~/utils/random.server';
+// import forge from 'node-forge';
+
+// const globalPepper = '<special_chars_here>'; // also used for AES256, should save it in a .key file or database
+
+/**
+ * @param raw raw password + salt
+ */
+export const hash = (raw: string, saltRounds = 10) => bcryptHash(raw, saltRounds);
+
+/**
+ * @param raw raw password + salt
+ * @param hash password string in database
+ */
+export const compare = bcryptCompare;
+
+export const getSaltedPasswd = async (passwd: string, salt?: string) => {
+  if (!passwd || typeof passwd !== 'string') throw new Error('"passwd" argument is invalid');
+  salt = salt || (await getStrongCryptoRandomStr());
+  return {
+    passwd: passwd + salt,
+    salt,
+  };
+};
--- app/utils/random.server.ts
+++ app/utils/random.server.ts
@@ -1,1 +1,7 @@
-export const getStrongCryptoRandomStr = () => {};
+import { randomBytes } from 'node:crypto';
+import { promisify } from 'node:util';
+
+const DEFAULT_CRYPTO_STR_LENGTH = 16;
+
+export const getStrongCryptoRandomStr = (size = DEFAULT_CRYPTO_STR_LENGTH, strType: BufferEncoding = 'hex') =>
+  promisify(randomBytes)(size).then((buf) => buf.toString(strType));
