9548810c120454bedaea23e6e83044bd8a8042dc
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2025-01-31 16:31:40 +0700

    feat(utils): implement `data` util helpers & tests

---
+++ app/utils/data.test.ts
@@ -0,0 +1,350 @@
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import {
+  checkIsEmpty,
+  checkIsFunction,
+  checkIsPlainObject,
+  convertMapToObject,
+  convertObjectToMap,
+  debounce,
+  decodeBase64,
+  encodeBase64,
+  identity,
+  omit,
+  pick,
+  shuffle,
+  uniq,
+  uniqBy,
+  uniqWith,
+  validateInstance,
+} from './data';
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+describe('encodeBase64()', () => {
+  it('should work correctly', () => {
+    expect(encodeBase64('')).toBe('');
+    expect(encodeBase64('ấịơ')).toBe('4bql4buLxqE=');
+    expect(encodeBase64('ab')).toBe('YWI=');
+    expect(encodeBase64('abc')).toBe('YWJj');
+    expect(encodeBase64('abcd')).toBe('YWJjZA==');
+  });
+});
+
+describe('decodeBase64()', () => {
+  it('should work correctly', () => {
+    expect(decodeBase64('')).toBe('');
+    expect(decodeBase64('4bql4buLxqE=')).toBe('ấịơ');
+    expect(decodeBase64('YWI=')).toBe('ab');
+    expect(decodeBase64('YWJj')).toBe('abc');
+    expect(decodeBase64('YWJjZA==')).toBe('abcd');
+  });
+});
+
+describe('convertObjectToMap()', () => {
+  it('should throw exception when value is invalid', () => {
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertObjectToMap(null)).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertObjectToMap(undefined)).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertObjectToMap('undefined')).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertObjectToMap('')).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertObjectToMap(11)).toThrow();
+  });
+
+  it('should return Map instance', () => {
+    expect(convertObjectToMap({}) instanceof Map).toBe(true);
+    expect(convertObjectToMap({})).toEqual(new Map());
+  });
+
+  it('should convert plain object to Map instance correctly', () => {
+    expect(
+      convertObjectToMap({
+        a: 1,
+        b: 'c',
+        d: null,
+        e: undefined,
+        f: false,
+      }),
+    ).toEqual(
+      new Map<string, any>([
+        ['a', 1],
+        ['b', 'c'],
+        ['d', null],
+        ['e', undefined],
+        ['f', false],
+      ]),
+    );
+  });
+});
+
+describe('convertMapToObject()', () => {
+  it('should throw exception when value is invalid', () => {
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertMapToObject(null)).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertMapToObject(undefined)).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertMapToObject('undefined')).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertMapToObject('')).toThrow();
+    // @ts-expect-error Testing invalid argument
+    expect(() => convertMapToObject(11)).toThrow();
+  });
+
+  it('should return plain object instance', () => {
+    expect(convertMapToObject(new Map()) instanceof Object).toBe(true);
+    expect(checkIsPlainObject(convertMapToObject(new Map()))).toBe(true);
+    expect(convertMapToObject(new Map())).toEqual({});
+  });
+
+  it('should convert Map instance to plain object correctly', () => {
+    expect(
+      convertMapToObject(
+        new Map<string, any>([
+          ['a', 1],
+          ['b', 'c'],
+          ['d', null],
+          ['e', undefined],
+          ['f', false],
+        ]),
+      ),
+    ).toEqual({
+      a: 1,
+      b: 'c',
+      d: null,
+      e: undefined,
+      f: false,
+    });
+  });
+});
+
+describe('checkIsPlainObject()', () => {
+  it('should return true when value is plain object', () => {
+    expect(checkIsPlainObject({})).toBe(true);
+    expect(checkIsPlainObject(new Object())).toBe(true);
+    expect(checkIsPlainObject(Object.create(null))).toBe(true);
+  });
+
+  it('should return false when value is not plain object ({})', () => {
+    expect(checkIsPlainObject('')).toBe(false);
+    expect(checkIsPlainObject('string')).toBe(false);
+    expect(checkIsPlainObject(1)).toBe(false);
+    expect(checkIsPlainObject(0)).toBe(false);
+    expect(checkIsPlainObject(null)).toBe(false);
+    expect(checkIsPlainObject(undefined)).toBe(false);
+    expect(checkIsPlainObject(Object.create(Array))).toBe(false);
+    expect(checkIsPlainObject(Object)).toBe(false);
+    expect(checkIsPlainObject([])).toBe(false);
+    // eslint-disable-next-line @typescript-eslint/no-array-constructor
+    expect(checkIsPlainObject(new Array())).toBe(false);
+    expect(checkIsPlainObject(new Map())).toBe(false);
+  });
+});
+
+describe('checkIsEmpty()', () => {
+  it('should return true when value is null or undefined', () => {
+    expect(checkIsEmpty([])).toBe(true);
+    expect(checkIsEmpty('')).toBe(true);
+    expect(checkIsEmpty(null)).toBe(true);
+    expect(checkIsEmpty(undefined)).toBe(true);
+    // TODO expect(checkIsEmpty(Object.create(null))).toBe(true);
+  });
+
+  it('should return false when object has prototype properties but no own properties', () => {
+    class Proto {
+      constructor() {}
+    }
+    // Proto.prototype.prop = 'value';
+    const obj = new Proto();
+    expect(checkIsEmpty(obj)).toBe(false);
+  });
+
+  it('should return false for objects with non-enumerable properties', () => {
+    const obj = Object.create(null);
+    obj.nonEnumProp = 'value';
+    expect(checkIsEmpty(obj)).toBe(false);
+  });
+});
+
+describe('checkIsFunction()', () => {
+  it('should return true when value is function/constructor/class', () => {
+    expect(checkIsFunction(Object)).toBe(true);
+  });
+
+  it('should return false when value is NOT function/constructor/class', () => {
+    expect(checkIsFunction('')).toBe(false);
+    expect(checkIsFunction('string')).toBe(false);
+    expect(checkIsFunction(1)).toBe(false);
+    expect(checkIsFunction(0)).toBe(false);
+    expect(checkIsFunction(null)).toBe(false);
+    expect(checkIsFunction(undefined)).toBe(false);
+    expect(checkIsFunction(Object.create(Array))).toBe(false);
+    expect(checkIsFunction([])).toBe(false);
+    expect(checkIsFunction([])).toBe(false);
+    expect(checkIsFunction(new Map())).toBe(false);
+  });
+});
+
+describe('pick()', () => {
+  it('should work correctly', () => {
+    expect(pick({ b: 1, a: 2 }, ['a'])).toStrictEqual({ a: 2 });
+  });
+
+  // TODO more tests for edge cases
+});
+
+describe('omit()', () => {
+  it('should work correctly', () => {
+    expect(omit({ b: 1, a: 2 }, ['a'])).toStrictEqual({ b: 1 });
+  });
+
+  // TODO more tests for edge cases
+});
+
+describe('uniq helpers', () => {
+  let origArr: any[], uniqArr: any[];
+  beforeEach(() => {
+    origArr = [false, 1, false, true, 2, 1, '', 'true', null, {}];
+    uniqArr = [false, 1, true, 2, '', 'true', null, {}];
+  });
+
+  describe('uniq()', () => {
+    it('should work correctly', () => {
+      expect(uniq(origArr)).toEqual(uniqArr);
+    });
+
+    // TODO more tests for edge cases
+  });
+
+  describe('uniqBy()', () => {
+    it('should work correctly', () => {
+      expect(uniqBy([], identity, Object.is)).toEqual([]);
+      expect(uniqBy([1], identity, Object.is)).toEqual([1]);
+      expect(uniqBy([1, 1], identity, Object.is)).toEqual([1]);
+    });
+
+    // TODO more tests for edge cases
+  });
+
+  describe('uniqWith()', () => {
+    it('should work correctly', () => {
+      expect(uniqWith(origArr, (a, b) => a === b)).toEqual(uniqArr);
+    });
+
+    // TODO more tests for edge cases
+  });
+});
+
+describe('debounce()', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it('should work correctly', () => {
+    const fn = vi.fn();
+    const debouncedFn = debounce(fn, 500);
+
+    debouncedFn();
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    vi.advanceTimersByTime(500);
+    expect(fn).toHaveBeenCalledTimes(1);
+    debouncedFn();
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    vi.advanceTimersByTime(500);
+    expect(fn).toHaveBeenCalledTimes(2);
+  });
+
+  // TODO test all available behavior options
+});
+
+describe('shuffle()', () => {
+  it('should work correctly', () => {
+    const origArr = [1, 'a', false];
+    const shuffledArr = shuffle(origArr);
+    origArr.forEach((item) => expect(shuffledArr).toContain(item)); // new array must contain all items of original array
+
+    const atLeastOneItemIndexHasChanged = origArr.reduce<boolean>((acc, item, index) => {
+      const itemIndexInShuffledArray = shuffledArr.findIndex((i) => i === item);
+      if (itemIndexInShuffledArray === -1) throw new Error('item not found in shuffled array');
+      return itemIndexInShuffledArray !== index || acc;
+    }, false);
+    expect(atLeastOneItemIndexHasChanged).toBe(true); // index of items in original array must be different
+    expect(shuffledArr.length).toBe(origArr.length);
+  });
+
+  it('should work correctly with object-like items', () => {
+    const origArr = [{ order: 1 }, new Set([1, 2, 3]), new Date('2020-01-01')];
+    const shuffledArr = shuffle(origArr);
+    origArr.forEach((item) => expect(shuffledArr).toContain(item)); // new array must contain all items of original array
+
+    const atLeastOneItemIndexHasChanged = origArr.reduce<boolean>((acc, item, index) => {
+      const itemIndexInShuffledArray = shuffledArr.findIndex((i) => i === item);
+      if (itemIndexInShuffledArray === -1) throw new Error('item not found in shuffled array');
+      return itemIndexInShuffledArray !== index || acc;
+    }, false);
+    expect(atLeastOneItemIndexHasChanged).toBe(true); // index of items in original array must be different
+    expect(shuffledArr.length).toBe(origArr.length);
+  });
+
+  // TODO more tests for edge cases
+});
+
+//   it('check true positive cases:', () => {
+//     expect(
+//       hasDuplicatedObjects([
+//         { a: 2, x: 'y' },
+//         { x: 'y', a: 2 },
+//       ]),
+//     ).toBe(true);
+//     expect(hasDuplicatedObjects([{ a: 2, x: 'y' }, { a: 2 }, { x: 'y', a: 2 }])).toBe(true);
+//   });
+
+//   it('check special cases', () => {
+//     expect(hasDuplicatedObjects([])).toBe(false);
+//     expect(hasDuplicatedObjects([{ a: 2 }])).toBe(false);
+//   });
+// });
+
+describe('validateInstance()', () => {
+  it('should NOT throw error if an object is an instance of a class', () => {
+    class A {}
+    class B extends Array {}
+    const a = new A();
+    const b = new B();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance({}, Object)).not.toThrow();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance([], Array)).not.toThrow();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance(/a/, RegExp)).not.toThrow();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance(a, A)).not.toThrow();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance(a, Object)).not.toThrow();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance(b, Array)).not.toThrow();
+  });
+
+  it('should throw error if an object is NOT an instance of a class', () => {
+    class A {}
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance({}, A)).toThrow();
+    // @ts-expect-error intentionally passing invalid arg type
+    expect(() => validateInstance(new A(), Array)).toThrow();
+  });
+});
---
+++ app/utils/data.ts
@@ -0,0 +1,283 @@
+import _intersectionWith from 'lodash.intersectionwith';
+
+export { default as checkIsEqual } from 'fast-deep-equal';
+
+export const encodeBase64 = (str: string) => Buffer.from(str).toString('base64');
+
+export const decodeBase64 = (str: string) => Buffer.from(str, 'base64').toString();
+
+export const convertObjectToMap = (obj: Record<string, any>) => {
+  if (!(obj instanceof Object)) throw new Error();
+  return new Map(Object.entries(obj));
+};
+
+export const convertMapToObject = <K extends string | number | symbol, V>(map: Map<K, V>): { [k: string]: V } => {
+  if (!(map instanceof Map)) throw new Error();
+  return Object.fromEntries(map);
+};
+
+export const convertMapToArray = <K, V>(map: Map<K, V>): [K, V][] => Array.from(map);
+
+/**
+ * Alternative of `lodash.isObjectLike`
+ *
+ * @see https://github.com/lodash/lodash/blob/97d4a2fe193a66f5f96d7b813bed96b35b5abf15/src/isObjectLike.ts
+ */
+export const checkIsObjectLike = (value: any): value is Record<string, any> =>
+  typeof value === 'object' && value !== null;
+
+/**
+ * Alternative of `lodash.isPlainObject`
+ *
+ * @see https://github.com/lodash/lodash/blob/0b28b7f7b6702c4bffe80f2d12279ff8df217116/src/isPlainObject.ts
+ */
+export const checkIsPlainObject = <T>(value: T): value is NonNullable<T> => {
+  if (!checkIsObjectLike(value) || getTag(value) !== '[object Object]') {
+    return false;
+  }
+  if (Object.getPrototypeOf(value) === null) {
+    return true;
+  }
+  let proto = value;
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+  return Object.getPrototypeOf(value) === proto;
+};
+
+/** Alternative of `lodash.isEmpty` */
+export const checkIsEmpty = (value: any) => {
+  if (value == null) {
+    return true;
+  }
+
+  if (Array.isArray(value) || typeof value === 'string') {
+    return value.length === 0;
+  }
+
+  if (value instanceof Set || value instanceof Map) {
+    return value.size === 0;
+  }
+
+  if (typeof value === 'object') {
+    // Check for empty objects, considering objects with prototype properties
+    return Object.keys(value).length === 0 && value.constructor === Object;
+  }
+
+  // Check for rare case where toString has been overridden to return an empty string
+  // if (typeof value.toString === 'function' && value.toString() === '') {
+  //   return true;
+  // }
+
+  return false;
+};
+
+/** Alternative of `lodash.isFunction` */
+export const checkIsFunction = <T extends (...args: any[]) => any>(value: any): value is T =>
+  typeof value === 'function';
+
+/**
+ * Alternative of `lodash.pick`
+ *
+ * PERF: In real usecases, the AMOUNT OF KEYS IS RELATIVELY SMALL (<10), `Array.prototype.reduce` performs better than
+ * `Object.fromEntries`. Ref: https://jsbench.me/2klropmx7y/1
+ *
+ * ```
+ * pick<T extends object, U extends keyof T>(object: T, ...props: Array<Many<U>>): Pick<T, U>;
+ * ```
+ */
+export const pick = <T extends Record<string, any>, U extends keyof T>(
+  obj: T,
+  keys: U[],
+): T extends object ? Pick<T, U> : null =>
+  checkIsObjectLike(obj)
+    ? Object.entries(obj) // will throw exception if `obj` is `null`
+        .filter(([key]) => keys.includes(key as U))
+        .reduce(
+          (acc, [key, value]) => {
+            // Assign key/value pair directly is always better than `Object.assign` in perf
+            acc[key as U] = value;
+            return acc;
+          },
+          {} as T extends object ? Pick<T, U> : null,
+        )
+    : (null as T extends object ? Pick<T, U> : null);
+
+/**
+ * Alternative of `lodash.omit`
+ *
+ * NOTE(perf): In real usecases, the AMOUNT OF KEYS IS RELATIVELY SMALL (<10), `Object.fromEntries().filter()` is better
+ * perf than `Array.prototype.reduce` in general (it scales well with either small/large number of key/value pairs in
+ * `obj`). Ref: https://jsbench.me/2klropmx7y/1
+ */
+export const omit = <T extends Record<string, any>, U extends keyof T>(obj: T, keys: U[]): Omit<T, U> =>
+  Object.fromEntries(Object.entries(obj).filter(([key]) => !keys.includes(key as U))) as Omit<T, U>;
+
+/** Alternative of `lodash.identity`. It return firstly passed argument. */
+export const identity = <T>(...args: [T, ...any]): T => args[0];
+
+/** Alternative of `lodash.uniq` */
+export const uniq = <T>(arr: T[]): typeof arr => Array.from(new Set(arr));
+
+/** Alternative of `lodash.uniqBy` with optional "comparator" (idea from `lodash.uniqWith`) */
+export const uniqBy = <T, U>(
+  arr: T[],
+  /** Receive being-checked item, return whatever you want to be checked */
+  iteratee: (a: T) => U,
+  /**
+   * This parameter must be required otherwise `uniq` is the better choice. Use `Object.js` in case you just want common
+   * strict compare function like `(a, b) => a === b`
+   */
+  comparator: (a: U, b: U) => boolean = Object.is,
+): typeof arr =>
+  arr.reduce<typeof arr>((acc, val, index) => {
+    if (arr.findLastIndex((a) => comparator(iteratee(a), iteratee(val))) === index) acc.push(val);
+    return acc;
+  }, []);
+
+/** Alternative of `lodash.uniqWith` */
+export const uniqWith = <T>(arr: T[], comparator: (a: T, b: T) => boolean): typeof arr =>
+  arr.reduce<typeof arr>((acc, val) => {
+    if (acc.findIndex((u) => comparator(val, u)) === -1) acc.push(val);
+    return acc;
+  }, []);
+
+/**
+ * @example
+ *   Setting options:
+ *   leading: true (trailing:false) -> before timeout
+ *   leading: false (trailing:false) -> after timeout
+ *   trailing: true (leading:true|false) -> [default] after timeout, used for typing (input's change) event handlers
+ */
+export const debounce = <T extends any[]>(
+  func: (...args: T) => any,
+  /** Time period in miliseconds */
+  wait: number,
+  {
+    leading = false,
+    trailing = true,
+    context,
+  }: {
+    leading?: boolean;
+    trailing?: boolean;
+    context?: any;
+  } = {
+    leading: false,
+    trailing: true,
+  },
+) => {
+  let timeout: ReturnType<typeof setTimeout> | number | null = null;
+
+  return function (...args: Parameters<typeof func>) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    context = context || this;
+
+    clearTimeout(timeout as number);
+
+    if (!timeout && leading && !trailing) {
+      func.apply(context, args);
+    }
+
+    timeout = setTimeout(() => {
+      timeout = null;
+      if (trailing || !leading) {
+        func.apply(context, args);
+      }
+    }, wait);
+  };
+};
+
+/** Alternative of `lodash.shuffle`. It shuffles array's items keeping their references */
+export const shuffle = <T>(arr: T[]): T[] => {
+  const copyArray = arr.slice();
+  let currentIndex = copyArray.length,
+    randomIndex,
+    temporaryValue;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    do {
+      randomIndex = Math.floor(Math.random() * copyArray.length);
+    } while (randomIndex === currentIndex - 1);
+
+    currentIndex--; // loop breaker; make its value in first loop valid for next swap:
+
+    // And swap it with the current element.
+    temporaryValue = copyArray[currentIndex];
+    copyArray[currentIndex] = copyArray[randomIndex];
+    copyArray[randomIndex] = temporaryValue;
+
+    // Reset
+    randomIndex = undefined;
+    temporaryValue = undefined;
+  }
+
+  return copyArray;
+};
+
+/**
+ * NOTE: `getCommonItems` is memoized due to the fact that both `getCommonItems` & `checkHasCommonItem` will be called together (in the same function) most of the times, so doing the same task outside & inside of `checkHasCommonItem` is a bit redundant.
+ *
+ * NOTE: `memoize-one` does not convert types well (the type of return value is `unknown[]` instead of `string[]` if both arrays' type is `string[]`).
+ */
+// export const getCommonItems = memoizeOne(_intersectionWith);
+// export const checkHasCommonItem = <T1 = unknown, T2 = unknown>(
+//   arr1: T1[] = [],
+//   arr2: T2[] = [],
+//   comparator: Parameters<typeof _intersectionWith>[2],
+// ): boolean => {
+//   if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length > 0 && arr2.length > 0) {
+//     const commonItems = getCommonItems(arr1, arr2, comparator);
+//     return commonItems.length > 0;
+//   }
+//   return false;
+// };
+
+/**
+ * This helper check whether an array contains duplicated objects (objects that have same properies & values)
+ */
+// export const hasDuplicatedObjects = (list: any[] = [], compareObjectFn = checkIsEqual) => {
+//   if (!Array.isArray(list)) throw new Error('First argument must be array');
+//   if (!checkIsFunction(compareObjectFn)) throw new Error('Second argument must be function');
+//   if (list.length < 2) return false;
+
+//   const uniqueList = uniqWith(list, compareObjectFn);
+//   // const uniqueList = [];
+//   // list.forEach((item) => {
+//   //   let isMatched = false;
+//   //   for (let i = 0; i < uniqueList.length; i++) {
+//   //     if (compareObjectFn(uniqueList[i], item)) {
+//   //       isMatched = true;
+//   //       return true;
+//   //     }
+//   //   }
+//   //   if (!isMatched) {
+//   //     uniqueList.push(item);
+//   //   }
+//   // });
+//   return uniqueList.length !== list.length;
+// };
+
+export const validateInstance = <T extends FunctionConstructor>(
+  object: any,
+  classConstructor: FunctionConstructor,
+): object is T => {
+  if (!(object instanceof classConstructor)) {
+    throw new Error(`Passed object is not an instance of ${classConstructor.name}`);
+  }
+  return object;
+};
+
+/**
+ * Gets the `toStringTag` of `value`.
+ *
+ * @see https://github.com/lodash/lodash/blob/97d4a2fe193a66f5f96d7b813bed96b35b5abf15/src/.internal/getTag.ts
+ */
+function getTag(value: any): string {
+  if (value == null) {
+    return value === undefined ? '[object Undefined]' : '[object Null]';
+  }
+  return Object.prototype.toString.call(value);
+}
