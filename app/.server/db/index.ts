61c78a629595bf89a240aea2a9dc1bc24bf66a9d
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2025-03-16 18:21:35 +0700

    feat(db): support creating pooled connection in order to use transaction

--- app/.server/db/index.ts
+++ app/.server/db/index.ts
@@ -4,16 +4,19 @@
 // - For Pool connection (uses websocket): https://neon.tech/guides/drizzle-local-vercel
 // - For serverless (uses fetch): https://neon.tech/guides/local-development-with-neon
 
-import { neonConfig } from '@neondatabase/serverless';
-import { drizzle as neonDrizzle } from 'drizzle-orm/neon-http';
+import { neonConfig, Pool } from '@neondatabase/serverless';
+import { drizzle as neonDrizzle } from 'drizzle-orm/neon-serverless'; // use `'drizzle-orm/neon-http'` for normal HTTP connection on serverless platforms
 import invariant from 'tiny-invariant';
+import ws from 'ws';
 
 import '~/.server/utils/import-env';
 
 invariant(process.env.DATABASE_URL, 'DATABASE_URL is not set');
 const connectionString = process.env.DATABASE_URL;
 
-// neonConfig.webSocketConstructor = ws; // for Pool connection
+neonConfig.webSocketConstructor = ws; // for Pool connection
+
+// NOTE: must use `process.env` instead of `import.meta.env` since `import.meta.env` is not available in custom scripts run with `node` (see `package.json`). See also: https://vite.dev/guide/env-and-mode#node-env-and-modes
 if (process.env.NODE_ENV === 'production') {
   // @ts-expect-error `EdgeRuntime` is defined only on Vercel's edge runtime
   if (typeof EdgeRuntime !== 'undefined') {
@@ -21,19 +24,19 @@ if (process.env.NODE_ENV === 'production') {
     neonConfig.poolQueryViaFetch = true; // to work in edge environments (Cloudflare Workers, Vercel Edge, etc.)
   }
 } else {
-  // For offline local development:
+  // Setting up offline local development:
+  // 1. For Fetch (HTTP) connection:
   neonConfig.fetchEndpoint = (host) => {
     const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
     return `${protocol}://${host}:${port}/sql`;
   };
-  // For Pool connection, use this instead of setting `fetchEndpoint`:
+  // 2. For Pool (WebSocket) connection:
   // const connectionStringUrl = new URL(connectionString);
-  // neonConfig.wsProxy = (host) => `${host}:4444/v1`;
-  // neonConfig.webSocketConstructor = ws;
-  // neonConfig.useSecureWebSocket = false;
-  // neonConfig.pipelineTLS = false;
-  // neonConfig.pipelineConnect = false;
-  // neonConfig.forceDisablePgSSL = true;
+  neonConfig.wsProxy = (host) => `${host}:4444/v1`;
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.pipelineTLS = false;
+  neonConfig.pipelineConnect = false;
+  neonConfig.forceDisablePgSSL = true;
 }
 
 export const db = neonDrizzle({
@@ -44,3 +47,15 @@ export const db = neonDrizzle({
   },
   casing: 'snake_case',
 });
+
+export const createPooledDBConnection = () => {
+  const pool = new Pool({
+    connectionString,
+    max: process.env.DB_MIGRATING || process.env.DB_SEEDING ? 1 : undefined,
+  });
+  const db = neonDrizzle(pool, {
+    ...neonConfig,
+    casing: 'snake_case',
+  });
+  return { pool, db };
+};
