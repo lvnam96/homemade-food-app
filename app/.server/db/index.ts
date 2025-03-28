6e53954e92ac38b7d871348380611f63329111aa
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2025-03-28 21:46:16 +0700

    refactor(db): unify setting up `drizzle`-`neon`-`postgres` driver

--- app/.server/db/index.ts
+++ app/.server/db/index.ts
@@ -4,9 +4,12 @@
 // - For Pool connection (uses websocket): https://neon.tech/guides/drizzle-local-vercel
 // - For serverless (uses fetch): https://neon.tech/guides/local-development-with-neon
 
+// Drizzle with Neon:
+// - https://orm.drizzle.team/docs/connect-neon
+// - https://neon.tech/docs/serverless/serverless-driver#use-the-driver-over-websockets
+
 import { neonConfig, Pool } from '@neondatabase/serverless';
 import { drizzle as neonServerlessDrizzle } from 'drizzle-orm/neon-serverless'; // use `'drizzle-orm/neon-http'` for normal HTTP connection on serverless platforms
-import { drizzle as neonHttpDrizzle } from 'drizzle-orm/neon-http'; // use `'drizzle-orm/neon-http'` for normal HTTP connection on serverless platforms
 import invariant from 'tiny-invariant';
 import ws from 'ws';
 
@@ -40,13 +43,12 @@ if (process.env.NODE_ENV === 'production') {
   neonConfig.forceDisablePgSSL = true;
 }
 
-export const db = neonHttpDrizzle({
+export const db = neonServerlessDrizzle({
   connection: {
     connectionString,
-    // TODO: check if this works (`neonDrizzle` has not supported this yet according to its typing):
-    // max: process.env.DB_MIGRATING || process.env.DB_SEEDING ? 1 : undefined,
   },
   casing: 'snake_case',
+  ws,
 });
 
 export const createPooledDBConnection = () => {
@@ -54,9 +56,11 @@ export const createPooledDBConnection = () => {
     connectionString,
     max: process.env.DB_MIGRATING || process.env.DB_SEEDING ? 1 : undefined,
   });
-  const db = neonServerlessDrizzle(pool, {
+  const db = neonServerlessDrizzle({
+    client: pool,
     ...neonConfig,
     casing: 'snake_case',
+    ws,
   });
   return { pool, db };
 };
--- app/.server/db/migrate.ts
+++ app/.server/db/migrate.ts
@@ -1,5 +1,5 @@
-import { migrate as neonMigrate } from 'drizzle-orm/neon-http/migrator';
-import { db } from './index';
+import { migrate as neonMigrate } from 'drizzle-orm/neon-serverless/migrator';
+import { createPooledDBConnection } from './index';
 import invariant from 'tiny-invariant';
 import drizzleConfig from '../../../drizzle.config';
 
@@ -12,9 +12,12 @@ invariant(process.env.DB_MIGRATING === 'true', 'DB_MIGRATING must be set to true
 const main = async () => {
   try {
     if (process.env.NODE_ENV === 'production') {
+      const { db, pool } = createPooledDBConnection();
       await neonMigrate(db, { migrationsFolder });
+      await pool.end();
     }
     console.log('Migration completed');
+    process.exit(0);
   } catch (error) {
     console.error('Error during migration:', error);
     process.exit(1);
